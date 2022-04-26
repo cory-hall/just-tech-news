f26ebb71bbcc5b7104e10823881d30e1afb20987
Cory Hall <ComebackCory1@gmail.com>
2022-04-26 17:02:18 -0500

    added password verification

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -50,6 +50,28 @@ router.post('/', (req, res) => {
       });
 });
 
+router.post('/login', (req, res) => {
+   // expects {email: 'email@email.com', password: 'password1234'}
+   User.findOne({
+      where: {
+         email: req.body.email
+      }
+   })
+      .then(dbUserData => {
+         if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+         }
+
+         const validPassword = dbUserData.checkPassword(req.body.password);
+         if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+         }
+         res.json({ user: dbUserData, message: 'You are now logged in!' });
+      });
+});
+
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
    // expects {username: 'Username', email: 'email@email.com', password: 'password1234}
@@ -81,17 +103,17 @@ router.delete('/:id', (req, res) => {
          id: req.params.id
       }
    })
-   .then(dbUserData => {
-      if (!dbUserData) {
-         res.status(404).json({ message: 'No user found with this id' });
-         return;
-      }
-      res.json(dbUserData);
-   })
-   .catch(err =>{
-      console.log(err);
-      res.status(500).json(err);
-   });
+      .then(dbUserData => {
+         if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+         }
+         res.json(dbUserData);
+      })
+      .catch(err => {
+         console.log(err);
+         res.status(500).json(err);
+      });
 });
 
 module.exports = router;
